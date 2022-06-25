19130d42ffed704b58f9014a05f58ff1d372c95c
OmarCorralHoyos <o.corral.h@gmail.com>
2022-06-25 04:32:16 -0500

    config variables del entorno

--- database/db.js
+++ database/db.js
@@ -10,7 +10,7 @@ const conexion = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME, 
-    port:process.env.DB_PORT,
+    port:"5432",
     ssl:{rejectUnauthorized: false}
 });
 
